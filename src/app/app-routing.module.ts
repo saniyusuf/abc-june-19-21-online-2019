1023092b569ebb95fa4f92284cca18f9bf17ef1e
Sani Yusuf <saniyusuf@gmail.com>
2019-06-20 11:12:00 -0500

    step 201

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  {
+    path: "dashboard",
+    loadChildren: () =>
+      import("./dashboard/dashboard.module").then(
+        module => module.DashboardModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { VideoDashboardComponent } from "./video-dashboard/video-dashboard.component";
+import { VideoListComponent } from "./video-list/video-list.component";
+import { StatFiltersComponent } from "./stat-filters/stat-filters.component";
+import { VideoPlayerComponent } from "./video-player/video-player.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const childRoutes: Routes = [{ path: "", component: VideoDashboardComponent }];
+
+@NgModule({
+  declarations: [
+    VideoDashboardComponent,
+    VideoListComponent,
+    StatFiltersComponent,
+    VideoPlayerComponent
+  ],
+  imports: [CommonModule, RouterModule.forChild(childRoutes)]
+})
+export class DashboardModule {}
